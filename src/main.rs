ab4adf8d2dd4eeb4a4d90bbe4276c8bc943cda83
Urmzd Mukhammadnaim <urmzd.mukhamadnaim@reeldata.ai>
2023-10-31 16:16:25 -0400

    fix: add ability to split at desired frames

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,19 @@
+use chrono::{self, Utc};
 use gst::{
     glib::{self, RustClosure, Value},
     prelude::*,
-    MessageView, State,
+    MessageView, PadProbeType, State,
 };
 use gstreamer as gst;
-use std::{io, sync::{atomic::AtomicU32, Arc, Mutex}, thread, time::Duration};
+use std::{
+    io,
+    sync::atomic::{AtomicU32, Ordering},
+    thread,
+    time::Duration,
+};
 use termion::{input::TermRead, raw::IntoRawMode};
 use tracing::{error, info};
 
-// Algorithm:
-//  Global:
-//      - frame_buffer
-//  Thread 1:
-//      - produce png frames into frame_buffer
-//      - on 300 frames, send_frames_to_collector(frames[frame_start..=frame_end])
-//  Thread 2:
-//  the collector should do the following:
-//      - create mp4 from png files
-//      - in prallel:
-//          - hash frames
-//      on hash_complete:
-//          - create video_segment_metadata
-//      on_mp4_complete:
-//          - upload video_segment to storage
-//          - upload video_segment_metadata to storage
-//
 pub const DEFAULT_RTSP_URI: &str = "localhost:8554/vsa";
 pub const FRAMES: u32 = 300;
 
@@ -70,34 +59,67 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     info!("RTSP URI: {}", rtsp_uri);
 
+    let video_format = "h264";
 
     // Elements
     let src = gst::ElementFactory::make("rtspsrc").build()?;
-    let depay = gst::ElementFactory::make("rtph265depay").build()?;
+    //let buffer = gst::ElementFactory::make("rtpjitterbuffer").build()?;
+    let depay = gst::ElementFactory::make(&format!("rtp{}depay", &video_format)).build()?;
+    let parse = gst::ElementFactory::make(&format!("{}parse", &video_format)).build()?;
+    let sink = gst::ElementFactory::make("splitmuxsink").build()?;
+    //let decode = gst::ElementFactory::make(&format("avdec_{}", &video_format)).build()?;
+    //let convert = gst::ElementFactory::make("videoconvert").build()?;
+    //let encoder = gst::ElementFactory::make("x264enc").build()?;
+
+    // disable automatic splits
     // Ensures that we're reading from the default stream.
     src.set_property_from_str("location", &rtsp_uri);
     src.set_property_from_str("protocols", "tcp");
-
-    let parse = gst::ElementFactory::make("h265parse").build()?;
-    //let decode = gst::ElementFactory::make("avdec_h265").build()?;
-    //let convert = gst::ElementFactory::make("videoconvert").build()?;
-    //let encoder = gst::ElementFactory::make("x264enc").build()?;
-    let sink = gst::ElementFactory::make("splitmuxsink").build()?;
-
-    let max_size_time = Duration::from_secs(1);
-
-    sink.set_property_from_str("max-size-time", &max_size_time.as_nanos().to_string());
-    sink.set_property_from_str("location", "frames/output%05d.mp4");
+    sink.set_property_from_str("max-size-time", "0");
+    sink.set_property_from_str("max-size-bytes", "0");
+    //sink.set_property_from_str("location", "segments/segment-%05d.mp4");
 
     let pipeline = gst::Pipeline::with_name("test-pipeline");
     let links = [
-        &src, &depay, &parse, //&decode,
+        &src,
+        //&buffer,
+        &depay,
+        &parse,
+        //&decode,
         //&convert,
         //&encoder,
         &sink,
     ];
     pipeline.add_many(&links)?;
     gst::Element::link_many(&links[1..])?;
+
+    let frame_count = AtomicU32::new(0);
+    let sink_clone = sink.clone();
+
+    let parse_src_pad = parse
+        .static_pad("sink")
+        .expect("sink pad to exist on parse");
+
+    parse_src_pad
+        .add_probe(
+            PadProbeType::BUFFER | PadProbeType::PUSH,
+            move |_pad, _pad_probe| {
+                let current_frame_idx = frame_count.load(Ordering::Relaxed);
+
+                info!("we are on frame {}", current_frame_idx);
+
+                if current_frame_idx == FRAMES {
+                    sink_clone.emit_by_name::<()>("split-after", &[]);
+                    info!("we have requested to split");
+                    frame_count.store(0, Ordering::Relaxed);
+                } else {
+                    frame_count.fetch_add(1, Ordering::Relaxed);
+                };
+
+                gst::PadProbeReturn::Ok
+            },
+        )
+        .expect("probe callback to have been added");
 
     src.connect_pad_added(move |src, src_pad| {
         src.downcast_ref::<gst::Bin>()
@@ -123,33 +145,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     });
 
-    // we add video names to this buffer
-    let video_buffer = Arc::new(Mutex::new(Vec::new()));
     let counter = AtomicU32::new(0);
-
-    let video_buffer_clone = Arc::clone(&video_buffer);
-
-    // spawn a thread that consumes frames from the video_buffer_clone
 
     sink.connect_closure(
         "format-location-full",
         false,
-        RustClosure::new(move |value| {
-            let internal_sink = value.get(0).unwrap().get::<gst::Element>();
-            let fragment_id = value.get(1).unwrap().get::<u32>().unwrap();
-            let sample = value.get(2).unwrap().get::<gst::Sample>().unwrap();
+        RustClosure::new(move |_value| {
+            //let fragment_id = value.get(1).unwrap().get::<u32>().unwrap();
+            //let sample = value.get(2).unwrap().get::<gst::Sample>().unwrap();
 
             let new_count = counter.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
 
-            let new_location = format!("frames/frame{}.mp4", new_count);
-            let mut buffer = video_buffer_clone.lock().unwrap();
-            buffer.push(new_location.clone());
+            let new_location = format!("segments/segment-{}.mp4", new_count);
 
             let new_location_value: Value = new_location.into();
             let ret_value = Some(new_location_value);
 
-            error!("fragment_id: {}", fragment_id);
-            error!("sample: {:?}", sample);
+            // log current time utc
+            error!("{}", Utc::now());
+            //error!("fragment_id: {}", fragment_id);
+            //error!("sample: {:?}", sample);
 
             ret_value
         }),
@@ -190,7 +205,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _bus_watch = bus.add_watch(move |_, msg| {
         match msg.view() {
             MessageView::Element(element) => {
-                error!("Made Element {:?}", element);
+                info!("received element {:?}", element);
             }
             _ => {}
         }
@@ -204,3 +219,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+//struct Publisher {}
+//impl Publisher {
+//// args: video_path
+//fn publish() {}
+//}
+//struct FrameBuffer {}
+//impl FrameBuffer {
+//// args: frame
+//fn add_frame() {}
+//}
+//// video buffer contains a list of videos that get translated into frames
+//struct VideoBuffer {}
